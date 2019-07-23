91ce6d409d6fd14e126f71c717dafa75e013680c
mcebisim <mcebisim@sahomeloans.com>
2019-07-23 15:08:43 +0200

    WIP

--- Source/Sales.Api/Handlers/UserRegisteredHandler.cs
+++ Source/Sales.Api/Handlers/UserRegisteredHandler.cs
@@ -2,15 +2,22 @@ using System;
 using System.Threading.Tasks;
 using Common.Events;
 using Common.IEvents;
+using Microsoft.Extensions.Logging;
 
 namespace Sales.Api.Handlers
 {
     public class UserRegisteredHandler : IEventHandler<UserRegistered>
     {
+        private readonly ILogger _logger;
+
+        public UserRegisteredHandler(ILogger logger)
+        {
+            _logger = logger;
+        }
         public async Task HandleAsync(UserRegistered @event)
         {
             await Task.CompletedTask;
-            Console.WriteLine($"User registered: {@event.FirstName}");
+            _logger.LogInformation($"User registered: {@event.FirstName}");
         }
     }
 }
